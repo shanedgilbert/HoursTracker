801f0a997de66abc71714025f2e92ac46f34b140
Shane Gilbert <35649752+shanedgilbert@users.noreply.github.com>
2021-03-17 20:47:12 -0700

    Updated and added FXML

---
+++ src/HourTrackerController.java
@@ -0,0 +1,25 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class HourTrackerController {
+    @FXML
+    private Button generateButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Button selectFileButton;
+
+    @FXML
+    private TextArea outputTextField;
+
+    @FXML
+    private TextArea statusTextField;
+
+    @FXML
+    private TextField inputFileName;
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,6 +12,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import org.apache.commons.compress.utils.InputStreamStatistics;
 import org.apache.poi.ss.formula.functions.T;
 
@@ -23,9 +25,7 @@ import org.apache.poi.ss.formula.functions.T;
 //}
 
 public class Main extends Application {
-    Button calcHoursButton;
 
-    //Main driver
     public static void main(String[] args) {
         launch(args);
     }
@@ -33,6 +33,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Hour Tracker");
+
+        Parent root = FXMLLoader.load(getClass().getResource("HourTrackerFXML.fxml"));
 
 //        TextField fileNameField = new TextField("Enter schedule file name.");
 //        HBox fileNameInputBox = new HBox();
@@ -45,55 +47,21 @@ public class Main extends Application {
 //            ht.inputExcelFile();
 //        });
 
-        //Creates grid container for input field
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10, 10, 10, 10));
-        grid.setVgap(5);
-        grid.setHgap(5);
-
-        //File name input field
-        final TextField fileName = new TextField();
-        fileName.setPromptText("Enter name of schedule.");
-        fileName.setPrefColumnCount(10);
-        fileName.getText();
-        GridPane.setConstraints(fileName, 0, 0);
-        grid.getChildren().add(fileName);
-
-        //Calculate button
-        Button generate = new Button("Generate");
-        GridPane.setConstraints(generate, 1, 0);
-        grid.getChildren().add(generate);
-
-        //Clear button
-        Button clear = new Button("Clear");
-        GridPane.setConstraints(clear, 1, 1);
-        grid.getChildren().add(clear);
-
-        //Text area for output
-        TextArea output = new TextArea();
-        GridPane.setConstraints(output, 2, 0);
-
         //Setting action for generate button
         //TODO
-        generate.setOnAction(e -> {
-            if(fileName.getText() == null || fileName.getText().isEmpty()) {
-                output.setText("Please enter a valid file name\n" +
-                        "ie: FINAL SCHEDULE.xlsx\n");
-            }
-            else {
-                HourTracker ht = new HourTracker(fileName.getText());
-                ht.buildMapFromRoster();
-                ht.inputExcelFile();
-            }
-        });
+//        generate.setOnAction(e -> {
+//            if(fileName.getText() == null || fileName.getText().isEmpty()) {
+//                output.setText("Please enter a valid file name\n" +
+//                        "ie: FINAL SCHEDULE.xlsx\n");
+//            }
+//            else {
+//                HourTracker ht = new HourTracker(fileName.getText());
+//                ht.buildMapFromRoster();
+//                ht.inputExcelFile();
+//            }
+//        });
 
-        //Setting action for clear button
-        //TODO
-
-        StackPane layout = new StackPane();
-        layout.getChildren().addAll(grid);
-
-        Scene scene = new Scene(layout, 600, 480);
+        Scene scene = new Scene(root, 640, 400);
         stage.setScene(scene);
         stage.show();
     }
