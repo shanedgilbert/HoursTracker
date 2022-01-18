941b85f8609694f361df3a9beb134c714048c922
Shane Gilbert <35649752+shanedgilbert@users.noreply.github.com>
2022-01-18 10:25:37 -0800

    Converted DOAStatus to use LinkedList. There is probably a better way to do this

--- src/DOAStatus.java
+++ src/DOAStatus.java
@@ -1,27 +1,48 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
 
 public class DOAStatus {
     //Instance variables
-    private final Map<String, String> studies;
+    LinkedList<String> dayList;
+    LinkedList<String> studyList;
 
     //Constructor to set empty study set
     public DOAStatus() {
-        studies = new HashMap<>();
+        dayList = new LinkedList<>();
+        studyList = new LinkedList<>();
     }
 
+    /**
+     * Records the conflicting study and its day
+     * @param day Day of conflict
+     * @param studyName Name of study with conflict
+     */
     public void addStudy(String day, String studyName) {
-        studies.put(day, studyName);
+        dayList.add(day);
+        studyList.add(studyName);
     }
 
-    //TODO: return the string: days with conflicts (from map)
+    /**
+     * Returns the string representation of the studies with DOA conflicts
+     * @return String representation of the studies with DOA conflicts
+     */
     public String getDays() {
-
+        StringBuilder output = new StringBuilder();
+        for(String s : dayList) {
+            output.append(s);
+        }
+        return output.toString();
     }
 
-    //TODO: return the string: studies (from map)
+    /**
+     * Returns the string representation of the days with DOA conflicts
+     * @return String representation of the days with DOA conflicts
+     */
     public String getStudies() {
-
+        StringBuilder output = new StringBuilder();
+        for(String s : studyList) {
+            output.append(s);
+        }
+        return output.toString();
     }
 
 }
